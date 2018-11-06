9315945f9bcdbb7f4e3b67b77b0ffd5726e01f1a
Avalos <raphael.avalos@telecom-paristech.fr>
2018-11-06 17:42:48 +0100

    Policy optimization

--- HW1/main.py
+++ HW1/main.py
@@ -3,6 +3,7 @@ import gridrender as gui
 import numpy as np
 import time
 import dynamicprogramming
+import policy_tools
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -18,7 +19,9 @@ v_q4 = np.array([[0.87691855, 0.92820033, 0.98817903, 0.00000000, 0.67106071, -0
 pol = [0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3]
 
 
+policy, Q, R, R_cummulated = policy_tools.policy_optimization(env)
 
-
-
+v_opt = [0.87691855, 0.92820033, 0.98817903, 0.00000000, 0.82369294, 0.92820033, 0.00000000, 0.77818504, 0.82369294,
+         0.87691855, 0.82847001]
+policy_tools.compare_value_function(Q.max(axis=2), v_opt)
 
